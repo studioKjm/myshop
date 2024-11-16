c736f170001500a8f5aa014c86eea600e5bc4780
jimin Kim <117446950+studioKjm@users.noreply.github.com>
2024-11-17 02:17:30 +0900

    11.17.1

--- myshop/settings.py
+++ myshop/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'shop.apps.ShopConfig',
+    'cart.apps.CartConfig',
 ]
 
 MIDDLEWARE = [
@@ -125,3 +126,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/' # p424
 MEDIA_ROOT = BASE_DIR / 'media/' # p424
+
+CART_SESSION_ID = 'cart' # p431
